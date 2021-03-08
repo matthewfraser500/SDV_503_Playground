95cf96b8431cbe9aab799fd02dcd41d421cb86da
Matthew Fraser <Matthew-Fraser@live.nmit.ac.nz>
2021-03-09 10:56:30 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ console.log('Matthew Fraser')
 //Declaring variables first name and assign a value string
 let firstName = 'Matthew'
 let lastName = 'Fraser'
-let fullName = firstName + ' ' + lastName 
-    fullName = `My full name is ${firstName} ${lastName}`
+let fullName = firstName + ' ' + lastName //ES5 
+    fullName = `My full name is ${firstName} ${lastName}` //ES6
 
 
 //Console.log firstName + lastName prints MatthewFraser into the terminal 
