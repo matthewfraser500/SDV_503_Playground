586e5d7fb023d2821ede8dd4aae90878a481d697
Matthew Fraser <Matthew-Fraser@live.nmit.ac.nz>
2021-03-09 10:55:34 +1300

    Introduction to variables

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//TODO Javascript Variables
 console.log('Hello SDV503')
 console.log('Matthew Fraser')
+
+//Declaring variables first name and assign a value string
+let firstName = 'Matthew'
+let lastName = 'Fraser'
+let fullName = firstName + ' ' + lastName 
+    fullName = `My full name is ${firstName} ${lastName}`
+
+
+//Console.log firstName + lastName prints MatthewFraser into the terminal 
+console.log(fullName)
+
