bcf204aefa3d68055a8d2edf3af721dd52253507
Matthew Fraser <Matthew-Fraser@live.nmit.ac.nz>
2021-03-16 10:10:12 +1300

    Learning if/else statements, understanding storing values using object

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//TODO Javascript Variables
+ /* //TODO Javascript Variables
 console.log('Hello SDV503')
 console.log('Matthew Fraser')
 
@@ -12,3 +12,48 @@ let fullName = firstName + ' ' + lastName //ES5
 //Console.log firstName + lastName prints MatthewFraser into the terminal 
 console.log(fullName)
 
+*/
+
+/* let x;
+
+if(x < 0) {
+    console.log('Ali')
+}
+else {
+    console.log('Error')
+}
+
+
+Function()
+
+console.log('error')
+
+x = "1234567"
+let y = Number(x)
+console.log(x)
+console.log(Number(x))
+console.log(typeof y)
+
+const a = 1 + 1
+console.log(a)
+let Matt
+console.log(Matt)
+
+const information = {
+    student: "Ali",
+    country: 'Iraq',
+    city: 'Nelson',
+    postcode: 7010
+}
+console.log(Object)
+
+*/
+
+const information = {
+    Name: 'Matthew',
+    Age: '24',
+    City: 'Nelson',
+    Occupation: 'Student'
+}
+
+console.log(information)
