a81c76f0b3d4948986c33fa796317175b1e2b46c
Matthew Fraser <Matthew-Fraser@live.nmit.ac.nz>
2021-03-23 11:02:27 +1300

    Update index.js

--- index.js
+++ index.js
@@ -181,3 +181,104 @@ const x = {
 const y = x
 
 console.log(y.city)
+
+//Week-5
+
+//For Loop Practice
+// This for loop is incrementing until it reaches 5
+for(i=0; i < 5; i++) {
+    console.log('This is a loop')
+}
+
+let arr = [1,2,3,4,5,6,7]
+
+for(i=0; i <= arr.length; i++) {
+    console.log('This is a loop')
+}
+
+//For Loop syntax
+
+//for (initialize; condition; increment);
+//for (initialize; condition; increment) single statement;
+//for (initialize; condition; increment) { multiple; statements; }
+
+// 0-index based counter
+// don't forget a break statement/condition
+for (let i = 0;; i++) {
+    console.log("loop, i = " + i);
+    if (i > 1)
+        break;
+}
+
+// Mulitple Statements
+/*let counter = 0;
+function inc() { counter++; }for (let i = 0; i < 10; i++, inc());
+console.log(counter);
+*/
+//Increment Numbers
+
+let counter = 0;
+for (let i = 0; i < 10; i++)
+    counter++;counter
+
+for (var i =0; i < 10; i++) {
+    let x = i;
+    console.log(x)
+}
+
+// Nested for Loops
+
+for (let y = 0; y < 2; y++)
+    for (let x = 0; x < 2; x++)
+        console.log(x, y);
+
+// Skipping Steps //
+// The if statement here is skipping number 5 //
+for ( let i = 0; i < 10; i++) { if (i == 5) continue; console.log(i );}
+
+// Breaking Early
+
+for ( let i = 0;; i++) { console.log('loop'); break;};
+
+// for of Loops And Strings
+
+/*The for...of statement creates a loop iterating over iterable objects, 
+/including: built-in String, Array, array-like objects (e.g., arguments or NodeList), TypedArray, Map, Set, and user-defined iterables. 
+It invokes a custom iteration hook with statements to be executed for the value of each distinct property of the object.
+*/
+
+
+let string = 'Matthew';
+for (let value of string)
+    console.log( value );
+
+let array = [0, 1, 2];
+
+for(let value of array)
+    console.log(value);
+
+
+//for of does !!NOT WORK with OBJECTS!!
+/*
+let object = {a: 1, b: 2, c: 3 };
+for {let value of object}
+console.log(value)
+*/
+
+//for in loops// ES6
+
+let object = { a: 1, b: 2, c: 3, method: () => { } };for (let value in object)
+    console.log(value, object[value]);
+
+//While loop
+
+let c = 0;
+while (c++ < 5)
+console.log(c);
+
+let y = 0;
+while (y++ < 1000) {
+    if (y > 1)
+    continue;
+    console.log(y);
+}
